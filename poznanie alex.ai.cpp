dfb49a52a18a9d05f99819da93dc3bec6b67e476
gutkowskialeksander47 <134397272+gutkowskialeksander47@users.noreply.github.com>
2024-12-26 17:20:42 +0100

    Update poznanie alex.ai.cpp

--- poznanie alex.ai.cpp
+++ poznanie alex.ai.cpp
@@ -1,7 +1,7 @@
 
 **Alex**
 
-Nazywam się [Aleksander Rajca]. Jestem pasjonatem technologii informacyjnych z 4 lat doświadczenia w branży IT. Specjalizuję się w programowaniu, administracji systemami, analizie danych, a moją misją jest tworzenie innowacyjnych rozwiązań technologicznych, które przynoszą wartość i ułatwiają życie użytkownikom.
+Nazywam się [Aleksander Rajca]. Jestem pasjonatem technologii informacyjnych z 4 latami doświadczenia w branży IT. Specjalizuję się w programowaniu, administracji systemami, analizie danych, a moją misją jest tworzenie innowacyjnych rozwiązań technologicznych, które przynoszą wartość i ułatwiają życie użytkownikom.
 
 **Zalety pracy w branży IT:**
 
