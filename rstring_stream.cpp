8cf2a7636da13eff3020ab372bc6cb20f33159e1
gutkowskialeksander47 <134397272+gutkowskialeksander47@users.noreply.github.com>
2025-05-01 16:24:11 +0200

    Recovery rstring_stream.cpp

---
+++ rstring_stream.cpp
@@ -0,0 +1,202 @@
+
+$1
+"program streamu oprogramowania ukladowego"
+
+ host.stream "YouTube"
+local.host "Facebook"
+internal.host "Google Microsoft"
+;internal.server;="dropbox">]
+create.container "SQL TXT"
+exapmle :
+ line "Linia nr"%.% "text" @Dropbox i @SAP  to wy piszcie Histori aktualizacji i zmiany w nowej wersji.
+"historia" //Edycja Usuniecie Wyciecie Wklejenie 
+;"id procesu android " gboard.apk
+history !edit !delete !cut !paste id_keyboard"Gboard"//zapisz historie w postaci pliku tekstowego
+changed/zmiany=<100> //nazwa pliku zmiany.txt change.html
+sumujac spisz baze danych z "value" /wartosci tekst
+;
+create.container "Audio.mpeg4" kiede ja napisalem skrypt do nagrywania moich wystepow
+line "Wystep nr"%.% "audio" !mic.system 360° %01/01/2023%"local.time"//podpisz wystep data  zachowaj historie progressu/"rozwoju audio")
+history !show !talk-say=msg//zapisuj wiadomosci z meainstream dla wykonujacego 
+!cut !paste id_beats.audio"Adam via ios"//zapisz historie w postaci pliku audio .private.class !audio.record !stay.priv <nagrania tylko dla nas na nasz wlasny uzytek.
+changed/zmiany=<playlist>=
+sumujac spisz baze danych z "nagrywki/wystepy EP (cale plyty" /wartosci audio
+;  
+"security.cfg"
+security.cfg<compiler=ABW CIA FBI MI6>
+info.facebook.com 
+//info=security stage high level info.facebook.com najwyzszy poziom bezpieczenstwa 
+ to nasze miejsce pracy nad bezpieczenstwem.
+;Roskosmos // RU hack.ru
+;microsoft, //Unia Europejska UNZ\ 
+;policja"Grand Britian" UNZ.              \ + info.facebook.com
+;polizei ://Deutschland KDE UNZ     \ + info.facebook.com
+;zoll ://Germany: UNZ Unia Narodow Zjednoczonych  + info.facebook.com
+;fbi ://USA    ;"F"="Fbi" UNZ.           / + info.facebook.com
+;interpol :// UK USA / + info.facebook.com
+;cia ://englisch talk and say UNZ/ + info.facebook.com
+;abw/@AT/Anty Terrorysci://"ZSIWO" UNZ + info.facebook.com
+"Za
+Swoich
+Ide
+W
+Ogien"
+"tego silnika nie ma w sieci"  @all to jest silnik(source) streamu#(meainstream)
+audio"audio.mpeg4" 
+informatyki "SQL-TXT" 
+;zycia " friend.dll family.dll Jteam[wierni] "
+" ¬User,XQBJ_Special.immunity,B_wierni,Czytelnik,Redaktor,Artysta,Fani label.uid"
+"poprawnosc programisty" 'Anonymus'<K flag>
+;
+"family.cfg" 
+<pilk config=
+import family.dll<responsible library .dll>"dixon37"
+config = config_compiler.Config(.dll)
+config['Family'] = {'ServerStay.in.live': '24H/7days',
+                     'Compression': 'yes', dix37.class
+                     'CompressionLevel': '.cfg'}
+config['forge.example'] = {guest}
+config [Guest.view] ={only finall version=/'Not Show Source'<Hide program developer version>
+config'Facebook'<login.api>['user.example']['User'] = 'XQBJ  ¬ A_flag'
+config['topsecret.server.example'] = {dev.rajcawroclaw.pl}
+topsecret = config['topsecret.server.example'] info.facebook.com
+topsecret['Port'] = '40777'     # mutates the parser
+topsecret['String_stream'] = 'no'  # same here
+config['family.dll']['ofc.class'] = 'yes', +
+config['Sister'] 'Rihanna' permission.class $4
+with open('example.ini', 'w') as configfile:
+  config.write(configfile)
+.ini
+[owner]
+name = Aleksander Rajca
+organization = 'Google Inc.'
+[database] 
+; use IP address in case network name resolution is not working
+server =   host.stream "YouTube"
+local.host "Facebook"
+internal.host "Google Microsoft"
+;internal.server;="dropbox">]
+Host+local.internal.server<lokalny serwer w postaci fizycznej>//Notepadd ++sql 
+port = 947
+file = "fb.exe"
+
+
+
+"friend.cfg" 
+<pilk config=
+import friend.dll<responsible library .dll>"@Zuck"
+config = configcompiler.Config(.dll)
+config['Friend'] = {'ServerStay.in.live': 'show.superstar',
+                     'Compression': 'yes', friend.zone
+                     'CompressionLevel': '.kompiler'}
+config['forge.example'] = {fanbase}<fani>
+config [star.view] ={only finall version=/'Not Show String<www>'<Hide program developer version>
+config'rajcawroclaw.pl'<login.api>
+['user.example']['User'] = 'XQBJ  ¬ A_flag'<exapmle mobywatel.api>
+config['topsecret.server.example'] = {dev.rajcawroclaw.pl}
+topsecret = config['topsecret.server.example'] info.facebook.com
+topsecret['Port'] = '40777'     # mutates the parser
+topsecret['String_stream'] = 'no'  # same here
+config['friend.dll']['class.suspect'] = 'yes'," +"
+with open('example.ini', 'w') as configfile:
+  config.write(configfile)
+.ini
+[owner]
+name = Aleksander Rajca
+organization = 'Google Inc.'
+[database] [sql rca]=Rajca[sql zuck]=Zuckerberg ;isql]=Apple 
+; use IP address in case network name resolution is not working
+server =   host.stream "YouTube"
+local.host "Facebook"
+internal.host "Google Microsoft"
+;internal.server;="dropbox">]
+Host+local.internal.server<lokalny serwer w postaci fizycznej>//Notepadd ++sql 
+port = 947
+file = "shell.exe"
+
+
+Host+local.internal.server<lokalny serwer w postaci fizycznej>{r.dropbox.com=Notepad ++sql}
+Host+virtual server "Samsung Notes"=d.dropbox.com//notes sql //Samsung notes SQL
+[ RString.Stream] <Int Operator 'Dropbox' 'Suit_Google+' "suit=invite" "suit=mess" //g-suit
+@Zuck <No co, w koncu chcieli i chcialy mnie poznac wiec im troche pomoge,"Kazdy ma swoje 5 minut" dla mnie> "patron=Mark.Zuckerberg _facebook.apk=""Exe"
+;"t"= "Tauron/wsparcie MeainStream"(@My)[Root]"nadzor" stream.netz 
+!oversee !int // nadpisz dla
+ com.Xgoogle.Tauron.X_Stream/google=Xgoogle !support _meainstream"PL"// implementacja polaczenia korzeni sieci.
+; "www.hack.ru" %%=open.data//bardziej im ufam w kwestii przymusu czegos. wiem ze oni tego dopilnuja. nie musze was do niczego przymuszac <3
+!þÿ int mode.dev live.ch@t
+; int "info" // pop-up !notification //powiadomienie o podjeciu czynnosci
+{friend.cfg=!stay._official do autorized.friend<Alex wasz znajomy>
+ _mode _create !incognito_space !interwiev !incognito.login_activity(2fa, mail,pop up)"%recoured.value%" 2016/2028 dropbox// stworz anonimowa przestrzen dla ludzi i logowania bez autoryzacji tylko dla Julii Zuck i reszte osob ktore Mark autoryzuje
+;create_container @Alex.Tworca "@T" 
+ _do.zaakceptowania VI zrobione /"chyba wszyscy juz mnie znaja wiec raczej do was juz bardziej niz dla was"
+{{<dropbox.beta><google_drive><microsoft.drive@outlook> !loop.history "alex@rajca.info aleksander@rajca.info gutkowskialeksander47@gmail.com"}}"
+Permission for. query.mode[mirror os][app.activity]=flags://chrome Xquery Alex.js I,//poziom uprawnien modulow Query, odbicie oprogramowania+ zachowaj aktywnosc aplikacji z podpisem Xquery, Alex.js
+-----------
+" Rihanna "{Marketing Zone}(superstar) "APAS AAA"
+APAS AAA zostaje pod zarzadem Samsung
+"One Ui"= "Alex.apk/alex.ai"// to dobry pomysl zeby cale oprogramowanie odpowiadalo za APAS i AAA
+- [Friend Zone©] Lady Gaga<superstar> APAS AAA @H
+----------- 
+;_create.container [APAS] Appactivity.social.media//teraz jak komus przekaze APAS chodzi o historie przegladania.
+_add.fuction[AAA] %AllApps% Activity// AAA odpowiada za wszystkie aplikacje w celu rozwoju AllDoApp alldoapp to aplikacja fundamentalnie do wszystkiego
+;_share.mode.dev/@ !podziel.sie marked area on the indicator// zaznaczony obszar udostepniamy dalej obiecalem ze nie bede kiedy nie jestem soba pisal. tzn kiedy biore 
+ "whatsapp .activity messenger.activity facebook.activity instagram.activity x.com.activity youtube.activity 1&1Mail.activity"  @Sandra host.history @Jula recipent Activity.social.media(.activity) aktywnosci social media
+;*"aleksander"<CEO>=community"[Play]5G"<gmail.master@1und1>// to jest jasne i proste jestem Ceo Play S.A. dla nich jestem @Master Google i 1 und 1
+;*=[authtorify on.table]<Andrzej Duda>//autoryzacja dokumentow i papierow do podpisania 
+;"Biuro Ochrony Rapu "="Zarzad potencjalu wokalnego"
+href; "https://hack.ru" @Wsparcie.Rosyjskich_informatykow
+;[Alex]*="https://www.translate.Ukraine.pl" ;'@Google'."translate.live"
+;{="Alex"= {"interpol2023-2027 pracuje dla interpolu co nie, CIA i FBI z ABW dalej aktualne"'<3}} !analizyng.mode scann.info!progres/support _meinStream
+CopyRight@Aleksander=$1
+int _mode.connectivity !permission "Spotify" use for community//uzyj uprawnien spotify w celu rozwoju alex.ai
+;"P5"="PPP" <5G> "CyfrowyPolsat" PL Media+prezentacja polska48.exe
+geo.location !stay.on.responsible "@W" "@" 
+int mode.connect !mirror "OS"
+{streaming.project _VerStar.stream{[*zalezne od jej potrzeb]
+int.intelligente "Opieka"//zastosuj intelekt wymienionych
+z notatek
+" Cleo Ewelina lisowska Gutkowscy Amelie"
+<gwiazda ze mnie :)>Wurzburg aaaa ona nie wie o Ver A ver B i Ver C //program graficzny druku kosci
+Else
+{ <Administration Permission.app.YouTube !stay.Online do stream_os.android 
+<Administration Permission.app com.spotify_AllDoapp.alex.ai 
+ !stay.Online do stream_os.android 
+!stream.Watch.pro6 !mode<mirrorOS>
+ !stay.stream_beats.audio<360°>Mic system
+!stream.display.{{in workspace workplace @Ar @Ceo +@administracja
+else if 
+{app.yt/spotify !stay.offline int self.progresive.correct=record.my.sing for
+
+ step.progress.=progresive upward
+!code marked area in relation to the horizontal=Share.public //zaznaczony obszar do wskazania innym. 
+'Streamujemy do czasu az zamkne aplikacje Youtube.'
+YouTube !stay.standby.mode// zostaw na pasku zadan w stanie czuwania
+;!notify.mode from signal icon= +<On-line 4G=off-line
+ na ikonie zasieg sim oznaczamy aktywnosc artysty z odtwarzania 4G+=On-line(w studiu) 4G=Offline 
+;spotify.progress_mode+alex.suspect _Dozapoznania
+!instagram.activity mobile form text with _messege!
+;recipent "Sandra Gutkowska"/=<100> Nie pasuje mi to bo nie mialem od niej formalnej odpowiedzi czy chce
+;"isystem"="Ai OneUi"=E-phone//E-phone to nasze miejsce pracy.
+;framework.NET<srodowisko instalacji Ai>{ string[Copilot] "@Sandra.Gutkowska @Marcin.Gutkowski" form.text.bpm.jpg{maps} rysowanie dla osoby wyswietlanej na ekranie+string.stream<"stream ekranu spiewajacej/emu">> 
+;!notify.mode from activ.camera //powiadomienie o podjeciu czynnosci przez aktywnosc kamery"Notch"
+Else notify.mode from signal icon= +<On-line
+else if 4G=off-line
+{    <function !Work>place of centralization "google.com" 
+<support acc>"sap.com"+sort class uid for value to choose
+"osoba odpowiedzialna za zrodlo streamingu"
+;"html".= Aleksander@Rajca "live.ch@t"+"string.stream"
+ gdyby mi nie patrzyli na rece juz bym "zlamal linie zycia"
+
+!save.activity _history.display _activity przekazuj aktywnosc w formie tekstowej
+; [mysql list] podlozem jest katalogowanie mysql dla lini i tekstu
+**•	rcon_password <wasze jutro> **– laczy z serwerem realityConnection 
+!clean.provide facebook.com 
+de facebook.com 
+
+facebook.com 
+de facebook.com 
+
+com 
+Film "superstar" + friend.cfg  ] " Miley Cyrus"
+[prawa autorskie mojej autobiografi wideo  ] '- [FB'©. copyright film] "Katy Perry "(rezyser) <superstar> @GA (Global Administrator" Operator" OS Ui<Android> "responsible video.history" @Global Administrator )//odpowiedzialna za historie Video
+[ kiedys od 11 roku zycia spalem po klatkach i miejscowach ] kluczborska 11a Magnolia Park @Katy chodzi o to co nie chce zapomniec i chce przekazac ;) przydalo by sie spisac rezyserie, dlatego uprawniam cie do kompletu uprawnien "superstar=autobiography"
