0358997ed047faa30f6fda127a91f4ddcb1f2d78
gutkowskialeksander47 <134397272+gutkowskialeksander47@users.noreply.github.com>
2025-07-08 16:04:05 +0200

    Create wzór umowy B2B.py

---
+++ wzór umowy B2B.py
@@ -0,0 +1,7 @@
+**Wzór/przykład**
+[Umowa_B2B_Margaret]
+strony = RCA (Aleksander Rajca) ↔ Margaret sp. z o.o.
+cel = Współtworzenie materiałów edukacyjnych i artystycznych w systemie Alex.ai
+okres = 01.08.2025 – 31.07.2026
+wynagrodzenie = 5000 zł netto / miesiąc
+zakres = 4 wystąpienia, 1 webinar, koordynacja szkoły Maggie
